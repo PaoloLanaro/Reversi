9dbc413ccd2cdf2507d6297623d02c18bae0dde7
paoloLanaro <lanaro.p@northeastern.edu>
2023-12-07 18:41:20 -0500

    updated factory pattern

--- Reversi Project Part 1/src/Reversi.java
+++ Reversi Project Part 1/src/Reversi.java
@@ -1,3 +1,4 @@
+import java.security.Provider;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +13,16 @@ import cs3500.reversi.model.players.Player;
 import cs3500.reversi.model.strategy.GoForCornersStrategy;
 import cs3500.reversi.model.strategy.MaxPointStrategy;
 import cs3500.reversi.provider.model.GenericPlayer;
+//import cs3500.reversi.provider.model.PlayerAiOne;
+//import cs3500.reversi.provider.model.PlayerAiThree;
 import cs3500.reversi.provider.model.ProviderModelAdapter;
 import cs3500.reversi.provider.model.ProviderPlayerAdapter;
+import cs3500.reversi.provider.model.ReadOnlyReversiModel;
+import cs3500.reversi.provider.model.TileType;
 import cs3500.reversi.provider.view.ReversiModelView;
 import cs3500.reversi.view.IView;
 import cs3500.reversi.view.ReversiGraphicsView;
+import cs3500.reversi.provider.view.ViewToProviderAdapter;
 
 /**
  * The main class for the Reversi project.
@@ -40,11 +46,12 @@ public final class Reversi {
     }
     MutableReversi model = new BasicReversi(gameSize);
     IView player1View = new ReversiGraphicsView(model);
-    List<Player> players = constructPlayers(args, gameSize);
-    ReversiController controller1 = new GameController(model, players.get(0), player1View);
+    Player player1 = constructPlayer1(args[1], gameSize);
+    ReversiController controller1 = new GameController(model, player1, player1View);
 
     ProviderModelAdapter adapter = new ProviderModelAdapter(gameSize);
-    ProviderPlayerAdapter player2 = new ProviderPlayerAdapter(adapter, DiscColor.WHITE);
+//    ProviderPlayerAdapter player2 = constructPlayer2(args[2], adapter);
+    ProviderPlayerAdapter player2 = constructPlayer2("playeraithree", adapter);
     ReversiModelView delegate = new cs3500.reversi.provider.view.ReversiGraphicsView(adapter,
             player2);
     ViewToProviderAdapter player2View = new ViewToProviderAdapter(delegate);
@@ -78,6 +85,33 @@ public final class Reversi {
 
     controller1.playGame();
 //    controller2.playGame();
+  }
+
+  private static Player constructPlayer1(String arg, int gameSize) {
+    DiscColor playerColor = DiscColor.BLACK;
+    switch (arg) {
+      case "human":
+        return new HumanPlayer(new BasicReversi(gameSize), playerColor);
+      case "maxpointstrat":
+        return new AIPlayer(playerColor, new MaxPointStrategy());
+      case "cornerstrat":
+        return new AIPlayer(playerColor, new GoForCornersStrategy());
+      default:
+        throw new IllegalArgumentException("Could not create player with argument " + arg);
+    }
+  }
+
+  private static ProviderPlayerAdapter constructPlayer2(String arg, ProviderModelAdapter model) {
+    TileType playerColor = TileType.WHITE;
+    DiscColor playercolor = DiscColor.WHITE;
+    switch (arg) {
+      case "playeraione":
+        return new ProviderPlayerAdapter(model, playercolor);
+      case "playeraithree":
+        return new ProviderPlayerAdapter(model, playercolor);
+      default:
+        throw new IllegalArgumentException("Could not create player with argument" + arg);
+    }
   }
 
   private static List<Player> constructPlayers(String[] args, int gameSize) {
